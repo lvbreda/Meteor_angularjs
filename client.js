08f7bc6b0e1b33e3552eb19f3d5d300f158506eb
lvbreda <lvbreda@gmail.com>
2012-11-03 02:10:24 +0100

    .findOne added

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ function clone(obj) {
 		return obj;
 	var copy = obj.constructor();
 	for (var attr in obj) {
-		if (obj.hasOwnProperty(attr) && attr!= '_id' && attr.indexOf('$') == -1)
+		if (obj.hasOwnProperty(attr) && attr != '_id' && attr.indexOf('$') == -1)
 			if ( typeof obj[attr] == "object") {
 				copy[attr] = clone(obj[attr]);
 			} else {
@@ -52,12 +52,16 @@ Meteor.AngularCollection = function(name, $scope) {
 	}
 	self.find = function(selector) {
 		query = self.collection.find(selector);
-		return new angularObjectCollection(self.$scope, self.name, query,selector);
+		return new angularObjectCollection(self.$scope, self.name, query, selector);
+	}
+	self.findOne = function(selector) {
+		query = self.collection.find(selector);
+		return new angularObjectCollection(self.$scope, self.name, query, selector, true);
 	}
 	self.insert = function(object) {
 		self.collection.insert(object);
 	}
-	self.emptyObject = function(){
+	self.emptyObject = function() {
 		return new angularObject(self.name, {}, $scope);
 	}
 	return self;
@@ -77,18 +81,26 @@ Array.prototype.$delete = function(id) {
 		_id : id
 	});
 }
-var angularObjectCollection = function($scope, name, query,selector) {
+var angularObjectCollection = function($scope, name, query, selector, single) {
 	var self = this;
 	self.value = new Array();
 	self.query = query;
 	self.name = name;
 	self.$scope = $scope;
 	self.selector = selector;
+	self.value.push(new angularObject(self.name, {}, self.$scope));
 	for (var i in self.query.collection.docs) {
 		try {
-			if(self.query.selector_f(self.query.collection.docs[i])){
-				var temp = new angularObject(self.name, query.collection.docs[i], $scope);
-				self.value.push(temp);
+			if (self.query.selector_f(self.query.collection.docs[i])) {
+
+				if (self.value.length == 1) {
+					for (o in query.collection.docs[i]) {
+						self.value[0][o] = query.collection.docs[i][o];
+					}
+				} else {
+					var temp = new angularObject(self.name, query.collection.docs[i], $scope);
+					self.value.push(temp);
+				}
 				$scope.$digest();
 			}
 		} catch(e) {
@@ -97,17 +109,25 @@ var angularObjectCollection = function($scope, name, query,selector) {
 	}
 	self.handle = self.query.observe({
 		added : function(object) {
-			if(!self.query.selector_f(object)){
+			if (!self.query.selector_f(object)) {
 				return;
 			}
-			for(i in self.value){
-				if(self.value[i]._id == object._id){
+			for (i in self.value) {
+				if (self.value[i]._id == object._id) {
 					return;
 				}
 			}
-			var temp = new angularObject(self.name, object, self.$scope);
-			if (_.indexOf(self.value, temp, true) == -1) {
-				self.value.push(temp);
+			if (self.value.length == 1) {
+				
+				for (o in object) {
+					self.value[0][o] = object[o];
+				}
+				console.log(self.value[0]);
+			} else {
+				var temp = new angularObject(self.name, object, self.$scope);
+				if (_.indexOf(self.value, temp, true) == -1) {
+					self.value.push(temp);
+				}
 			}
 			if (!$scope.$$phase)
 				$scope.$apply();
@@ -119,27 +139,31 @@ var angularObjectCollection = function($scope, name, query,selector) {
 				$scope.$apply();
 		},
 		changed : function(old_document, at_index, new_document) {
-				var change = false;
-				for(var i in self.value){
-					var temp = self.value[i];
-					if(temp._id == old_document._id){
-						for(var p in temp){
-							if(p.indexOf("$") == -1){
-								self.value[i][p] = old_document[p];
-								change = true;
-							}
+			var change = false;
+			for (var i in self.value) {
+				var temp = self.value[i];
+				if (temp._id == old_document._id) {
+					for (var p in temp) {
+						if (p.indexOf("$") == -1) {
+							self.value[i][p] = old_document[p];
+							change = true;
 						}
 					}
 				}
-				
-				if (change) {
-					if (!$scope.$$phase)
-									$scope.$apply();
-				}
+			}
+
+			if (change) {
+				if (!$scope.$$phase)
+					$scope.$apply();
+			}
 		}
 	});
+	if (!single) {
+		return self.value;
+	} else {
+		return self.value[0];
+	}
 
-	return self.value;
 }
 var angularObject = function(name, content, $scope, collection) {
 	var self = this;
@@ -159,14 +183,14 @@ var angularObject = function(name, content, $scope, collection) {
 		var self = this;
 		var temp = clone(self);
 		if ( temp instanceof Object && Meteor.AngularCollectionHolder[self.$collection]) {
-			if(!self._id){
+			if (!self._id) {
 				Meteor.AngularCollectionHolder[self.$collection].insert(temp);
-			}else{
-					Meteor.AngularCollectionHolder[self.$collection].update({
-						_id : self._id
-					}, temp);
+			} else {
+				Meteor.AngularCollectionHolder[self.$collection].update({
+					_id : self._id
+				}, temp);
 			}
-		
+
 		}
 
 	}
