8085a49f258c05af0109a71e62cc366240574417
Adam Ritter <aritter@gmail.com>
2012-11-28 18:41:17 -0500

    Refactor controller to be more event oriented Move filtering for tags to server side

--- examples/todos/client/app/controllers.js
+++ examples/todos/client/app/controllers.js
@@ -4,24 +4,57 @@ function MainCtrl($scope,$rootScope,$routeParams,$location,$timeout){
 	$scope.Lists = new Meteor.AngularCollection("lists",$scope);
 	$scope.Todos = new Meteor.AngularCollection("todos",$scope);
 	$scope.lists = $scope.Lists.find({});
-	$scope.selectedList = $routeParams.id;
-	$scope.selectedTags  = [];
-	$scope.newlist = {};
-	$scope.newTodo = {
-		tags : [],
-		list_id : $scope.selectedList
+	
+	$scope.currentTags = function(){
+	var temp = {};
+	for(var i in $scope.todos){
+		todo = $scope.todos[i];
+		for(var o in todo.tags){
+			tag = todo.tags[o];
+			if(temp[tag]){
+				temp[tag] = temp[tag] +1;
+			}else{
+				if(!(tag instanceof Object)){
+					temp[tag] = 1;
+				}
+				
+			}
+		}
 	}
+	return temp;
+}
+	$scope.$watch('selectedList', function() {
+		$scope.selectedTags = [];
+		$scope.newTodo = {
+			tags : [],
+			list_id : $scope.selectedList
+		}	
+	})
+	$scope.$watch('selectedTags + selectedList', function() {
+		if($scope.selectedList){
+			sel = {list_id : $scope.selectedList}
+			if ($scope.selectedTags.length > 0) {
+				sel.tags = {'$in' : $scope.selectedTags};
+			}
+			$scope.todos = $scope.Todos.find(sel);
+			
+			// This could also be done by a separate query in the previous function
+			if ($scope.selectedTags.length == 0) {
+				$scope.computedCurrentTags = $scope.currentTags();
+			}
+		} else {
+			$scope.todos = null;
+		}
+	})
+	$scope.selectedList = $routeParams.id;
+	$scope.oldSelectedList = $scope.selectedList;
+
 	/**Nasty hack**/
 	$scope.changeList = function(id){
 		$scope.stopped = false;
 		$scope.click = $timeout(function(){ 
 			if(!$scope.stopped)   {
 				 $scope.selectedList = id;
-				 $scope.todos = $scope.Todos.find({list_id : $scope.selectedList});
-				 $scope.newTodo = {
-						tags : [],
-						list_id : $scope.selectedList
-					}
 			}      
         },200);
 	}
@@ -29,44 +62,19 @@ function MainCtrl($scope,$rootScope,$routeParams,$location,$timeout){
 		$scope.stopped = $timeout.cancel($scope.click);
 	}
 	/**Stop nasty **/
-	$scope.currentTags = function(){
-		var temp = {};
-		for(var i in $scope.todos){
-			for(var o in $scope.todos[i].tags){
-				if(temp[$scope.todos[i].tags[o]]){
-					temp[$scope.todos[i].tags[o]] = temp[$scope.todos[i].tags[o]] +1;
-				}else{
-					if(!($scope.todos[i].tags[o] instanceof Object)){
-						temp[$scope.todos[i].tags[o]] = 1;
-					}
-					
-				}
-			}
-		}
-		return temp;
-	}
+
 	$scope.removeTag = function(todo,tag){
 		todo.tags.splice(todo.tags.indexOf(tag),1);
 		todo.$save();
 	}
-	$scope.filterByTag = function(todo){
-		for(i in $scope.selectedTags){
-			if(todo.tags.indexOf($scope.selectedTags[i]) >-1){
-				return true;
-			}
-		}
-		if($scope.selectedTags.length == 0){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	
 	$scope.addFilterTag = function(tag){
 		if($scope.selectedTags.indexOf(tag)>-1){
 			$scope.selectedTags.splice($scope.selectedTags.indexOf(tag),1);
 		}else{
 			$scope.selectedTags.push(tag);
 		}
+		$scope.selectedTags = clone($scope.selectedTags); // Force updating
 	}
 	$scope.isFilterTag = function(tag){
 		if($scope.selectedTags.indexOf(tag)>-1){
@@ -74,8 +82,4 @@ function MainCtrl($scope,$rootScope,$routeParams,$location,$timeout){
 		}
 		return false;
 	}
-	if($routeParams.id){
-		$scope.todos = $scope.Todos.find({list_id : $routeParams.id});
-	}
-	
 }
